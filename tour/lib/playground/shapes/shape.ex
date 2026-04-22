f7a736bc1b202efbcb1bdd2aadc9b53691e5b027
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-22 15:52:54 +0200

    docs: add iex impls tip and dialyzer notes to Shape protocol

--- tour/lib/playground/shapes/shape.ex
+++ tour/lib/playground/shapes/shape.ex
@@ -1,7 +1,16 @@
+# To get a list of implements in iex we can run
+# ```ex
+#  Shapes.Shape.__protocol__(:impls)
+# ```
 defprotocol Shapes.Shape do
   @doc "Return the area of a shape"
   # t() is an Elixir convention: the main type a module defines.
   @spec area(t()) :: number()
+
+  # `mix dialyzer` can be used to flag
+  #     - Callers passing the wrong type
+  #     - Impls returning the wrong type
+  #     - Unreachable patterns
 
   def area(shape)
 end
