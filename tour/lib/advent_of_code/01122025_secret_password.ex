e143b08f5173664cecb63b17ea236fc228881865
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-23 06:06:27 +0200

    feat: implement solve pipeline for SecretPassword with private helpers

--- tour/lib/advent_of_code/01122025_secret_password.ex
+++ tour/lib/advent_of_code/01122025_secret_password.ex
@@ -1,11 +1,25 @@
 defmodule SecretPassword do
-  # A file with a list of turns
-  def read_file(input_file) do
+  def solve(input_file) do
+    input_file
+    |> read_file()
+    |> extract_list_from_file_contents()
+    |> find_password()
   end
 
-  def extract_list_from_file_contents(file_contents) do
+  # A file with a list of turns separated by newlines.
+  # L10\nR20 etc...
+  defp read_file(input_file) do
+    File.read!(input_file)
   end
 
-  def find_password(list_of_turns) do
+  defp extract_list_from_file_contents(file_contents) do
+    file_contents
+    |> String.trim()
+    |> String.split("\n")
+  end
+
+  # The password is the number of times the dial has landed on the number 0
+  # The dial can also be turned from 0 to 99 and from 99 to 0
+  defp find_password(list_of_turns) do
   end
 end
