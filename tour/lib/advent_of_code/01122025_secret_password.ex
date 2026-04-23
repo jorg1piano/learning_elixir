ddc5573112ba9a57d5749a1e790f46b68bfaf5be
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-23 06:00:27 +0200

    feat: scaffold SecretPassword module with advent of code input

---
+++ tour/lib/advent_of_code/01122025_secret_password.ex
@@ -0,0 +1,11 @@
+defmodule SecretPassword do
+  # A file with a list of turns
+  def read_file(input_file) do
+  end
+
+  def extract_list_from_file_contents(file_contents) do
+  end
+
+  def find_password(list_of_turns) do
+  end
+end
