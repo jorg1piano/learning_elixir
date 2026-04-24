eff518a6ceb4d5c3176e2ee16b4e5336a43d9c8c
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-24 06:41:54 +0200

    feat: add Map.get example with default value to Maps module

--- tour/lib/playground/maps.ex
+++ tour/lib/playground/maps.ex
@@ -2,4 +2,6 @@ defmodule Maps do
   def new_literal, do: %{name: "Jorgen", age: 33}
   def tuple_constructed, do: Map.new([{:name, "Jorgen"}, {:age, 33}])
   def access_value_by_key, do: new_literal()[:name]
+  # Map.get/3 allows you to specify a default value if the key is not found.
+  def access_with_map_get, do: Map.get(new_literal(), :name, :not_found)
 end
