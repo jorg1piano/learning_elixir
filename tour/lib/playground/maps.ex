2a2d085bfdf75541906719dd2e2f148f63978d6a
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-24 06:40:23 +0200

    feat: add map key access example to Maps module

--- tour/lib/playground/maps.ex
+++ tour/lib/playground/maps.ex
@@ -1,4 +1,5 @@
 defmodule Maps do
   def new_literal, do: %{name: "Jorgen", age: 33}
   def tuple_constructed, do: Map.new([{:name, "Jorgen"}, {:age, 33}])
+  def access_value_by_key, do: new_literal()[:name]
 end
