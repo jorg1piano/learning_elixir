679bcc765c556fed9ab2d97c5e9166b8076b1ca5
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-24 06:43:28 +0200

    feat: add Map.fetch examples to Maps module

--- tour/lib/playground/maps.ex
+++ tour/lib/playground/maps.ex
@@ -4,4 +4,8 @@ defmodule Maps do
   def access_value_by_key, do: new_literal()[:name]
   # Map.get/3 allows you to specify a default value if the key is not found.
   def access_with_map_get, do: Map.get(new_literal(), :name, :not_found)
+
+  # Map.fetch/2 returns a tuple {:ok, value} if the key is found, or :error if it is not.
+  def access_with_fetch, do: Map.fetch(new_literal(), :name)
+  def access_with_fetch_not_found, do: Map.fetch(new_literal(), :non_existing_key)
 end
