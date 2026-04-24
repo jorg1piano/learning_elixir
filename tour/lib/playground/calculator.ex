4326e075b71baf9a241b94e471ea9e09226c96a2
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-24 05:40:41 +0200

    feat: add CalculatorShorthand with default argument syntax

--- tour/lib/playground/calculator.ex
+++ tour/lib/playground/calculator.ex
@@ -2,3 +2,9 @@ defmodule Calculator do
   def add(a, b), do: a + b
   def add(a), do: add(a, 0)
 end
+
+defmodule CalculatorShorthand do
+  # \\ generates a function with two arguments,
+  # where the second argument has a default value of 0
+  def add(a, b \\ 0), do: a + b
+end
