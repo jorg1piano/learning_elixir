6032865a16b6cd2026a43b6e088b4d35c1ca8c00
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-24 05:44:48 +0200

    feat: add Circle module demonstrating module attributes

--- tour/lib/playground/calculator.ex
+++ tour/lib/playground/calculator.ex
@@ -8,3 +8,9 @@ defmodule CalculatorShorthand do
   # where the second argument has a default value of 0
   def add(a, b \\ 0), do: a + b
 end
+
+defmodule Circle do
+  # @pi is a module attribute, which is a constant that can be used in the module
+  @pi 3.14159
+  def area(radius), do: @pi * radius * radius
+end
