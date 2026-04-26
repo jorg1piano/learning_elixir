eec1c8158e2018618d703139ab43e38ff6cae059
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-26 09:10:30 +0200

    docs: clarify caller perspective on function clauses

--- tour/lib/playground/function_clauses.ex
+++ tour/lib/playground/function_clauses.ex
@@ -1,6 +1,10 @@
 # When there are multiple functions of the same arity,
 # these are called function clauses.
 # Here we pass pattern match on a tuple passed in the area function.
+#
+# From the caller's perspective, this is a single function (area/1)
+# There is no way to target a specific version of this other than passing
+# the correct argument, which is pattern matched at runtime.
 defmodule Geometry do
   # area/1
   # Matches when we have 3-tuple and first argument is a rectangle
