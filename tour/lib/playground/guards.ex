a2467121284f1361d9152f78777714b8ceca38ed
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-26 09:29:37 +0200

    refactor: add is_number guard and reorder IsPositive clauses

--- tour/lib/playground/guards.ex
+++ tour/lib/playground/guards.ex
@@ -1,6 +1,6 @@
 defmodule IsPositive do
   # Guards it's an extension of the basic pattern matching mechanism.
-  def test(x) when x < 0, do: :negative
+  def test(x) when is_number(x) and x < 0, do: :negative
+  def test(x) when is_number(x) and x > 0, do: :postive
   def test(x) when x == 0, do: :zero
-  def test(x) when x > 0, do: :postive
 end
