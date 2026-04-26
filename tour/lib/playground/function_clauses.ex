ccc82c78f275ebc5b7d1a4da69379ade77b0bbb9
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-26 09:15:32 +0200

    refactor: use structs instead of tuples in Geometry function clauses

--- tour/lib/playground/function_clauses.ex
+++ tour/lib/playground/function_clauses.ex
@@ -5,20 +5,34 @@
 # From the caller's perspective, this is a single function (area/1)
 # There is no way to target a specific version of this other than passing
 # the correct argument, which is pattern matched at runtime.
+#
+# Invoked like `Geometry.area(%Geometry.Rectangle{a: 3, b: 4})`
 defmodule Geometry do
+  defmodule Rectangle do
+    defstruct [:a, :b]
+  end
+
+  defmodule Circle do
+    defstruct [:r]
+  end
+
+  defmodule Square do
+    defstruct [:a]
+  end
+
   # area/1
   # Matches when we have 3-tuple and first argument is a rectangle
-  def area({:rectangle, a, b}) do
+  def area(%Rectangle{a: a, b: b}) do
     a * b
   end
 
   # Matches when we have 2-tuple and first argument is a circle
-  def area({:circle, r}) do
+  def area(%Circle{r: r}) do
     r * r * 3.14
   end
 
   # Matches when we have 2-tuple and first argument is a square
-  def area({:square, a}) do
+  def area(%Square{a: a}) do
     a * a
   end
 end
