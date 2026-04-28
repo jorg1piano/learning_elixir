ba1221af4ba2cc86105f3f29fa0e9ee8cc3e2594
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-28 06:02:50 +0200

    test: group max_num tests with describe block

--- tour/test/case_expression_test.exs
+++ tour/test/case_expression_test.exs
@@ -2,15 +2,17 @@ defmodule CaseExpressionsTest do
   use ExUnit.Case
   doctest CaseExpressions
 
-  test "returns a when a == b" do
-    assert CaseExpressions.max_num(1, 1) == 1
-  end
+  describe "max_num" do
+    test "returns a when a == b" do
+      assert CaseExpressions.max_num(1, 1) == 1
+    end
 
-  test "returns b when a < b" do
-    assert CaseExpressions.max_num(0, 4) == 4
-  end
+    test "returns b when a < b" do
+      assert CaseExpressions.max_num(0, 4) == 4
+    end
 
-  test "returns a when a > b" do
-    assert CaseExpressions.max_num(5, 3) == 5
+    test "returns a when a > b" do
+      assert CaseExpressions.max_num(5, 3) == 5
+    end
   end
 end
