7a7b2ebcef35ed51deca107b43eae25c75abbe5b
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-28 05:50:24 +0200

    feat: add extract_user using with block to combine extractors

--- tour/lib/playground/with_branching.ex
+++ tour/lib/playground/with_branching.ex
@@ -1,4 +1,13 @@
 defmodule LoginHelper do
+  def extract_user(user) do
+    # The with block returns the value of the first clause that fails to match its pattern.
+    # If every clause matches, the do block is executed.
+    with {:ok, email} <- extract_email(user),
+         {:ok, password} <- extract_password(user) do
+      {:ok, %{email: email, password: password}}
+    end
+  end
+
   def extract_email(%{email: email}), do: {:ok, email}
   def extract_email(_), do: {:error, "email missing"}
 
--- tour/test/login_helper_test.exs
+++ tour/test/login_helper_test.exs
@@ -2,6 +2,17 @@ defmodule LoginHelperTest do
   use ExUnit.Case
   doctest LoginHelper
 
+  describe "extract user and email" do
+    creds = %{email: "test@test.no", password: "secret_password"}
+
+    expect = %{
+      email: "test@test.no",
+      password: "secret_password"
+    }
+
+    assert LoginHelper.extract_user(creds) == {:ok, expect}
+  end
+
   describe "extract email" do
     test "returns {:ok, email} when present in map" do
       creds = %{email: "test@test.no"}
