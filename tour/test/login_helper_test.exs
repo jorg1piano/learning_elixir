856dbe7f9accfd963954453786b6d7f1f055205c
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-28 05:40:19 +0200

    test: group LoginHelper tests with describe blocks

--- tour/test/login_helper_test.exs
+++ tour/test/login_helper_test.exs
@@ -2,21 +2,25 @@ defmodule LoginHelperTest do
   use ExUnit.Case
   doctest LoginHelper
 
-  test "returns email when present in map" do
-    creds = %{email: "test@test.no"}
-    assert LoginHelper.extract_email(creds) == {:ok, "test@test.no"}
+  describe "extract email" do
+    test "returns {:ok, email} when present in map" do
+      creds = %{email: "test@test.no"}
+      assert LoginHelper.extract_email(creds) == {:ok, "test@test.no"}
+    end
+
+    test "return {:error, reason} when email is missing" do
+      assert LoginHelper.extract_email(%{}) == {:error, "email missing"}
+    end
   end
 
-  test "return {:error, reason} when email is missing" do
-    assert LoginHelper.extract_email(%{}) == {:error, "email missing"}
-  end
+  describe "extract password" do
+    test "resturns {:ok, password} when present in map" do
+      creds = %{password: "secret_password"}
+      assert LoginHelper.extract_password(creds) == {:ok, "secret_password"}
+    end
 
-  test "resturns password when present in map" do
-    creds = %{password: "secret_password"}
-    assert LoginHelper.extract_password(creds) == {:ok, "secret_password"}
-  end
-
-  test "returns {:error, reason} when password is missing" do
-    assert LoginHelper.extract_password(%{}) == {:error, "password missing"}[j]
+    test "returns {:error, reason} when password is missing" do
+      assert LoginHelper.extract_password(%{}) == {:error, "password missing"}
+    end
   end
 end
