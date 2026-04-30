d866d0a00042bd47ccc42db979cc5757b2c9b8d6
Jørgen Andersen <jorg1piano@gmail.com>
2026-04-30 06:18:57 +0200

    feat: LambdaEnum.sum reduces list with captured +/2 operator

--- tour/lib/playground/lambda_enum.ex
+++ tour/lib/playground/lambda_enum.ex
@@ -19,4 +19,13 @@ defmodule LambdaEnum do
   def odd_numbers(list) do
     Enum.filter(list, &(rem(&1, 2) == 1))
   end
+
+  def sum(list) do
+    # &+/2 captures the + function (arity 2) as an anonymous function value,
+    # so it can be passed to Enum.reduce as the reducer.
+    # Equivalent to: Enum.reduce(list, 0, fn a, b -> a + b end)
+    # If I had my own add/2 function, the syntax would be: &add/2
+    # In dart terms this is like a "tear-off" function
+    Enum.reduce(list, 0, &+/2)
+  end
 end
--- tour/test/lambda_enum_test.exs
+++ tour/test/lambda_enum_test.exs
@@ -13,4 +13,8 @@ defmodule LambdaEnumTest do
   test "odd_numbers returns odd numbers" do
     assert LambdaEnum.odd_numbers([1, 2, 3, 4]) == [1, 3]
   end
+
+  test "sum sums the list" do
+    assert LambdaEnum.sum([2, 2, 4]) == 8
+  end
 end
