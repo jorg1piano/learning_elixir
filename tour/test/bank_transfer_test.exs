3ff7da65bcc179b464296dd0ec9436fa6c34368d
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-01 08:31:10 +0200

    test: group BankTransfer.transfer_money tests in describe block

--- tour/test/bank_transfer_test.exs
+++ tour/test/bank_transfer_test.exs
@@ -2,20 +2,22 @@ defmodule BankTransferTest do
   use ExUnit.Case
   doctest BankTransfer
 
-  test "transfer amount lesss than or equal the balance succeeds" do
-    assert BankTransfer.transfer_money("acc-1", "acc-2", 500) == {:ok, {:money_transfered, 500}}
-  end
+  describe "transfer_money" do
+    test "transfering amounts less than or equal the balance succeeds" do
+      assert BankTransfer.transfer_money("acc-1", "acc-2", 500) == {:ok, {:money_transfered, 500}}
+    end
 
-  test "transfer amounts higher than the balance fails" do
-    assert BankTransfer.transfer_money("acc-1", "acc-2", 501) == {:error, :insufficient_funds}
-  end
+    test "transfer amounts higher than the balance fails" do
+      assert BankTransfer.transfer_money("acc-1", "acc-2", 501) == {:error, :insufficient_funds}
+    end
 
-  test "passing negative amount fails" do
-    assert BankTransfer.transfer_money("acc-1", "acc-2", -1) == {:error, :invalid_amount}
-  end
+    test "passing negative amount fails" do
+      assert BankTransfer.transfer_money("acc-1", "acc-2", -1) == {:error, :invalid_amount}
+    end
 
-  test "passing string as amount fails" do
-    assert BankTransfer.transfer_money("acc-1", "acc-2", "should have been a number") ==
-             {:error, :invalid_amount}
+    test "passing string as amount fails" do
+      assert BankTransfer.transfer_money("acc-1", "acc-2", "should have been a number") ==
+               {:error, :invalid_amount}
+    end
   end
 end
