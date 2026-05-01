3ee4b3cecc7ea8e1cb8336d941cf8bc6660fd71f
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-01 21:06:29 +0200

    feat: add periodic state cleanup to KeyValueStoreV2

--- tour/lib/playground/processes/gen_server.ex
+++ tour/lib/playground/processes/gen_server.ex
@@ -1,6 +1,7 @@
 # iex(1)> {:ok, pid} = KeyValueStoreV2.start()
 # iex(2)> KeyValueStoreV2.put(pid, "age", 33)
 # iex(3)> KeyValueStoreV2.get(pid, "age")
+# A key-value store that deletes its internal state every 60 seconds
 defmodule KeyValueStoreV2 do
   use GenServer
 
@@ -9,7 +10,14 @@ defmodule KeyValueStoreV2 do
   def get(pid, key), do: GenServer.call(pid, {:get, key})
 
   # Genserver overrides
-  def init(_), do: {:ok, %{}}
+  def init(_) do
+    :timer.send_interval(60_000, :cleanup)
+    {:ok, %{}}
+  end
+
+  def handle_info(:cleanup, _state) do
+    {:noreply, %{}}
+  end
 
   def handle_cast({:put, key, value}, state) do
     {:noreply, Map.put(state, key, value)}
