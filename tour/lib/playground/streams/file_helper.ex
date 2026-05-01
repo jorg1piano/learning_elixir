59e304f6fbb1a90101411c665009635dfa354f58
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-01 10:51:45 +0200

    docs: explain single-pass streaming in FileHelper

--- tour/lib/playground/streams/file_helper.ex
+++ tour/lib/playground/streams/file_helper.ex
@@ -1,6 +1,8 @@
 defmodule FileHelper do
   def scan_lines_longer_than(file_path, length) do
     # Lazely do operations on a enumarable with a stream
+    # This does the operations in a single pass
+    # and never have to load the full file into memory
     File.stream!(file_path)
     |> Stream.map(&String.trim_trailing(&1, "\n"))
     |> Enum.filter(&(String.length(&1) > length))
