6259810eab99b80147dea0090e29a8c840a9bdfc
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-01 13:14:48 +0200

    feat: add add_entry and entries functions to TodoListV4

--- tour/lib/playground/abstractions/todo_list_v4.ex
+++ tour/lib/playground/abstractions/todo_list_v4.ex
@@ -2,4 +2,15 @@ defmodule TodoListV4 do
   defstruct next_id: 1, entries: %{}
 
   def new(), do: %TodoListV4{}
+
+  def add_entry(%TodoListV4{} = todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.next_id)
+    new_entries = Map.put(todo_list.entries, todo_list.next_id, entry)
+
+    %TodoListV4{todo_list | entries: new_entries, next_id: todo_list.next_id + 1}
+  end
+
+  def entries(%TodoListV4{} = todo_list) do
+    todo_list.entries
+  end
 end
--- tour/test/abstractions/todo_list_v4_test.exs
+++ tour/test/abstractions/todo_list_v4_test.exs
@@ -5,4 +5,14 @@ defmodule TodoListV4Test do
   test "new creates a empty todo list" do
     assert TodoListV4.new().entries == %{}
   end
+
+  test "adding a new item inserts the entry" do
+    assert TodoListV4.new()
+           |> TodoListV4.add_entry(%{title: "Item A"})
+           |> TodoListV4.add_entry(%{title: "Item B"})
+           |> TodoListV4.entries() == %{
+             1 => %{id: 1, title: "Item A"},
+             2 => %{id: 2, title: "Item B"}
+           }
+  end
 end
