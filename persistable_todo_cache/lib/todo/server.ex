790933a0ca7923f6ac7c0a3a558a00ff9d2abf97
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-02 09:31:21 +0200

    broken state commit while learning about database

--- persistable_todo_cache/lib/todo/server.ex
+++ persistable_todo_cache/lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start(name) do
+    GenServer.start(__MODULE__, nil, name: name)
   end
 
   def add_entry(pid, entry), do: GenServer.cast(pid, {:add_entry, entry})
@@ -10,17 +10,19 @@ defmodule Todo.Server do
   def entries(pid), do: GenServer.call(pid, :entries)
 
   @impl true
-  def init(_) do
-    {:ok, Todo.List.new()}
+  def init(name) do
+    {:ok, {name, Todo.List.new()}}
   end
 
   @impl true
-  def handle_cast({:add_entry, entry}, state) do
-    {:noreply, Todo.List.add_entry(state, entry)}
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    new_list = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_list)
+    {:noreply, {name, new_list}}
   end
 
   @impl true
-  def handle_call(:entries, _from, state) do
-    {:reply, Todo.List.entries(state), state}
+  def handle_call(:entries, _from, {_name, todo_list} = state) do
+    {:reply, Todo.List.entries(todo_list), state}
   end
 end
