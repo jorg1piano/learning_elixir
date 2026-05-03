31adc4f4df60b6634c7ae4176c6f3fb5a4b23c6c
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-03 07:50:56 +0200

    chore: add trapping_exit example to ProcessLink playground

--- tour/lib/playground/processes.ex
+++ tour/lib/playground/processes.ex
@@ -1,4 +1,5 @@
 # iex(0)|> ProcessLink.linkedProccess
+# iex(1)|> ProcessLink.trapping_exit
 defmodule ProcessLink do
   def linkedProccess() do
     spawn(fn ->
@@ -12,5 +13,25 @@ defmodule ProcessLink do
 
       raise("something went wrong")
     end)
+  end
+
+  def trapping_exit do
+    spawn(fn ->
+      # Sets a flag on the current process to "trap" the exit
+      # The exit becomes a regular message in the process inbox ({:EXIT, pid, reason}).
+      Process.flag(:trap_exit, true)
+
+      # This raised message gets sent to the mailbox
+      spawn_link(fn -> raise("Something went wrong") end)
+
+      receive do
+        # print the raised error
+        {:EXIT, pid, reason} ->
+          IO.inspect("Process #{inspect(pid)} crashed, reason: #{inspect(reason)}")
+      end
+    end)
+
+    Process.sleep(1000)
+    IO.puts("caller still alive")
   end
 end
