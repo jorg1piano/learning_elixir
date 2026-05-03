10d3c1cdeb9ed52052d7b84b2d33c6d7ca83997d
Jørgen Andersen <jorg1piano@gmail.com>
2026-05-03 08:33:28 +0200

    refactor: use Supervisor behaviour with init/1 callback in Todo.System

--- persistable_todo_cache/lib/todo/system.ex
+++ persistable_todo_cache/lib/todo/system.ex
@@ -1,11 +1,19 @@
 # Illustrates how the "system" using Supervisor
 # can be launched through a nice interface.
 # iex(1)> Todo.System.start_link()
+# iex(2)> Todo.Cache.server_process("bob's list")
+# iex(3)> Process.exit(Process.whereis(Todo.Cache), :kill)
+# iex(4)> Todo.Cache.server_process("bob's list")
+
 defmodule Todo.System do
+  use Supervisor
+
   def start_link do
-    Supervisor.start_link(
-      [Todo.Cache],
-      strategy: :one_for_one
-    )
+    # Start the supervisor as this __MODULE__ as the callback module see init/1
+    Supervisor.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    Supervisor.init([Todo.Cache], strategy: :one_for_one)
   end
 end
